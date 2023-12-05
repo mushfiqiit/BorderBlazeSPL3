4f86bb4c5d81451138c69d46c2a18fb3488ebb1b
Mushfiqur Rahman Chowdhury <48788177+mushfiqiit@users.noreply.github.com>
2023-12-05 18:51:57 +0600

    image exchange between frontend and backend

--- backend/main.py
+++ backend/main.py
@@ -1,41 +1,70 @@
-from fastapi import FastAPI, File, UploadFile, Form
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
+import matplotlib.pyplot as plt
+from io import BytesIO
+import numpy as np
 from fastapi.middleware.cors import CORSMiddleware
-from typing import Annotated
 
 app = FastAPI()
 
+# Fixed filename for the uploaded file
+uploaded_filename = "file1.png"
+
+# Allow all origins in development (for testing purposes)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-@app.get("/")
-async def main():
-    return {"message": "Hello World"}
-
-@app.post("/files/")
-async def create_file(file: bytes = File(...)):
-    return {"file_size": len(file)}
+def generate_empty_plot(x, y, z, colors):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x, y, z, c=colors)
+    ax.set_title("Input Point Cloud Plot")
+    ax.set_xlabel("X-axis")
+    ax.set_ylabel("Y-axis")
+    ax.set_zlabel("Z-axis")
+    buffer = BytesIO()
+    plt.savefig(buffer, format="png")
+    buffer.seek(0)
+    plt.close()
+    return buffer
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
-    contents = await file.read()
-    print(file.filename)
-    return {"filename": file.filename, "file_size": len(contents)}
+    try:
+        # Specify the destination path with the fixed filename
+        destination_path = f"uploads/{uploaded_filename}"
 
-@app.post("/files2/")
-async def create_file(
-    file: Annotated[bytes, File()],
-    fileb: Annotated[UploadFile, File()],
-    token: Annotated[str, Form()],
-):
-    return {
-        "file_size": len(file),
-        "token": token,
-        "fileb_content_type": fileb.content_type,
-    }
+        # Save the uploaded file with the fixed filename
+        with open(destination_path, "wb") as dest_file:
+            dest_file.write(file.file.read())
 
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
+    # Process your uploaded file (if needed)
+    content = await file.read()
+
+    # Use BytesIO to read the content
+    file_content = BytesIO(content)
+
+    # Load the point cloud data from the file
+    point_cloud_data = np.loadtxt(file_content, skiprows=1, max_rows=10000)
+
+    # Extract X, Y, Z coordinates from the point cloud data
+    x = point_cloud_data[:, 0]
+    y = point_cloud_data[:, 1]
+    z = point_cloud_data[:, 2]
+
+    # Create an array to hold the colors for each point
+    colors = ['green'] * len(point_cloud_data)
+
+    # Generate the empty plot
+    empty_plot_buffer = generate_empty_plot(x, y, z, colors)
+
+    # Return the empty plot as part of the API response
+    return StreamingResponse(empty_plot_buffer, media_type="image/png")
--- frontend/src/components/fileUpload.jsx
+++ frontend/src/components/fileUpload.jsx
@@ -1,46 +1,69 @@
-import React, { Component } from 'react';
-import PostWithHead from '../utils/postWithHead';
+import React, { useState } from 'react';
 
-class FileUpload extends Component {
-    state = { 
-        selectedFile:null
-     } 
+const FileUploadComponent = () => {
+  const [file, setFile] = useState(null);
+  const [imageSrc, setImageSrc] = useState(null);
 
-    fileSelectedHandler=event=>{
-        this.setState({
-            selectedFile:event.target.files[0]
-        })
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+    setImageSrc(null); // Clear previous image when a new file is selected
+  };
+
+  const handleUpload = async () => {
+    if (!file) {
+      console.error('Please select a file before uploading.');
+      return;
     }
 
-    fileUploadHandler=async()=>{
-        const fd=new FormData();
-        fd.append(
-            'image', this.state.selectedFile, 
-            this.state.selectedFile.name
-            )
-        const response=await PostWithHead(
-            "uploadfile/", fd, 
-        )
-        console.log(response);
-    }
+    const formData = new FormData();
+    formData.append('file', file);
 
-    render() {
-        return (
-            <div className='App'>
-    <label for="formFile" class="form-label">Upload Point Cloud Data File</label>
-  <input 
-  class="form-control" 
-  id="formFile" type="file" 
-  onChange={this.fileSelectedHandler}
-  />
-    <button type="button" class="btn btn-primary" 
-    onClick={this.fileUploadHandler}>
-    Submit
-    </button>
-  </div>
-            
-        );
+    try {
+      const response = await fetch('http://localhost:8000/uploadfile/', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        const contentType = response.headers.get('content-type');
+
+        if (contentType && contentType.includes('image/png')) {
+          // If the response is an image, convert it to a data URL
+          const blob = await response.blob();
+          const dataUrl = URL.createObjectURL(blob);
+          console.log(dataUrl);
+          setImageSrc(dataUrl);
+        } else {
+          console.error('Unexpected response content type:', contentType);
+        }
+      } else {
+        console.error('Failed to upload file:', response.status, response.statusText);
+      }
+    } catch (error) {
+      console.error('Error during file upload:', error);
     }
-}
- 
-export default FileUpload;
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload</button>
+
+      {imageSrc && (
+        <div>
+          <p>Uploaded Image:</p>
+          <img src={imageSrc} alt="Uploaded" style={{ maxWidth: '100%' }} />
+        </div>
+      )}
+
+      {imageSrc && (
+        <div>
+          <p>Uploaded Image:</p>
+          <img src={imageSrc} alt="Uploaded" style={{ maxWidth: '100%' }} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FileUploadComponent;
